9c4b7c82fa0b726dc6453a312ca806320713102b
Bruno Augusto <brunoacpe@gmail.com>
2025-11-23 18:37:51 -0300

    :white_check_mark: Adicionando testes unitarios do service.

--- src/test/java/com/brunopacheco/jwtvalidator/fixtures/Fixtures.java
+++ src/test/java/com/brunopacheco/jwtvalidator/fixtures/Fixtures.java
@@ -1,9 +1,23 @@
 package com.brunopacheco.jwtvalidator.fixtures;
 
+import com.brunopacheco.jwtvalidator.dto.JwtPayloadDto;
+
 public class Fixtures {
     public static final String POST_API_PATH = "/api/v1/validate/jwt";
     public static final String VALID_JWT_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJOYW1lIjoiVG9uaW5obyBBcmF1am8iLCJSb2xlIjoiQWRtaW4iLCJTZWVkIjoiNzg0MSJ9.2sy5IzDmC_1oGHd5Jq9sSHH8Bt6NzfZhV1Bu5-WiBmA";
     public static final String INVALID_JWT_TOKEN = "abc.def.ghi";
     public static final String VALID_TOKEN_BOOLEAN_RESPONSE = "verdadeiro";
     public static final String INVALID_TOKEN_BOOLEAN_RESPONSE = "falso";
+
+    public static class JwtPayloadDtoFixtures {
+
+        public static JwtPayloadDto validJwtPayloadDtoFixture() {
+            return new JwtPayloadDto("Toninho Araujo", "Admin", 7841);
+        }
+
+        public static JwtPayloadDto invalidJwtPayloadDtoSeedIsNotAPrimeNumberFixture() {
+            return new JwtPayloadDto("Name", "Admin", 10);
+        }
+
+    }
 }
---
+++ src/test/java/com/brunopacheco/jwtvalidator/service/JwtValidationServiceTest.java
@@ -0,0 +1,104 @@
+package com.brunopacheco.jwtvalidator.service;
+
+import com.brunopacheco.jwtvalidator.dto.JwtPayloadDto;
+import com.brunopacheco.jwtvalidator.exception.BadRequestException;
+import com.brunopacheco.jwtvalidator.fixtures.Fixtures;
+import com.brunopacheco.jwtvalidator.utils.JwtParser;
+import com.brunopacheco.jwtvalidator.utils.ValidatorUtil;
+import com.brunopacheco.jwtvalidator.validators.SeedValidator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class JwtValidationServiceTest {
+
+    @Mock
+    private JwtParser jwtParser;
+
+    @Mock
+    private ValidatorUtil validatorUtil;
+
+    @Mock
+    private SeedValidator seedValidator;
+
+    @InjectMocks
+    private JwtValidationService service;
+
+    @Test
+    void shouldValidateJwtSuccessfully() {
+        String jwt = Fixtures.VALID_JWT_TOKEN;
+
+        JwtPayloadDto payload = Fixtures.JwtPayloadDtoFixtures.validJwtPayloadDtoFixture();
+
+        when(jwtParser.parse(jwt)).thenReturn(payload);
+
+        String result = service.validateJwt(jwt);
+
+        assertEquals(Fixtures.VALID_TOKEN_BOOLEAN_RESPONSE, result);
+
+        verify(jwtParser).parse(jwt);
+        verify(validatorUtil).validate(payload);
+        verify(seedValidator).validate(7841);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenParserFails() {
+        when(jwtParser.parse(Fixtures.INVALID_JWT_TOKEN)).thenThrow(new BadRequestException("invalid token"));
+
+        BadRequestException ex =
+                assertThrows(BadRequestException.class, () -> service.validateJwt(Fixtures.INVALID_JWT_TOKEN));
+
+        assertEquals("invalid token", ex.getMessage());
+
+        verify(jwtParser).parse(Fixtures.INVALID_JWT_TOKEN);
+        verifyNoInteractions(validatorUtil);
+        verifyNoInteractions(seedValidator);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenValidatorUtilFails() {
+        String jwt = Fixtures.VALID_JWT_TOKEN;
+
+        JwtPayloadDto payload = new JwtPayloadDto("Name", "Admin", 11);
+
+        when(jwtParser.parse(jwt)).thenReturn(payload);
+        doThrow(new BadRequestException("jakarta failed"))
+                .when(validatorUtil).validate(payload);
+
+        BadRequestException ex =
+                assertThrows(BadRequestException.class, () -> service.validateJwt(jwt));
+
+        assertEquals("jakarta failed", ex.getMessage());
+
+        verify(jwtParser).parse(jwt);
+        verify(validatorUtil).validate(payload);
+        verifyNoInteractions(seedValidator);
+    }
+
+    @Test
+    void shouldThrowExceptionWhenSeedValidatorFails() {
+        String jwt = Fixtures.VALID_JWT_TOKEN;
+
+        JwtPayloadDto payload = Fixtures.JwtPayloadDtoFixtures.invalidJwtPayloadDtoSeedIsNotAPrimeNumberFixture();
+
+        when(jwtParser.parse(jwt)).thenReturn(payload);
+        doThrow(new BadRequestException("not prime"))
+                .when(seedValidator).validate(10);
+
+        BadRequestException ex =
+                assertThrows(BadRequestException.class, () -> service.validateJwt(jwt));
+
+        assertEquals("not prime", ex.getMessage());
+
+        verify(jwtParser).parse(jwt);
+        verify(validatorUtil).validate(payload);
+        verify(seedValidator).validate(10);
+    }
+}
