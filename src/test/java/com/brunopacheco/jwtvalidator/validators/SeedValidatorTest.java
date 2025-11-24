9b9c92c615d9a0ce2ae6d1b7e683689a7c33692c
Bruno Augusto <brunoacpe@gmail.com>
2025-11-24 01:19:42 -0300

    :white_check_mark: Criando testes unitarios para a classe SeedValidator.

---
+++ src/test/java/com/brunopacheco/jwtvalidator/validators/SeedValidatorTest.java
@@ -0,0 +1,98 @@
+package com.brunopacheco.jwtvalidator.validators;
+
+import com.brunopacheco.jwtvalidator.exception.BadRequestException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SeedValidatorTest {
+
+    private SeedValidator seedValidator;
+
+    @BeforeEach
+    void setup() {
+        seedValidator = new SeedValidator();
+    }
+    
+
+    @Test
+    void shouldPassWhenSeedIsPrime() {
+        assertDoesNotThrow(() -> seedValidator.validate("11"));
+    }
+
+    @Test
+    void shouldPassWhenSeedIsAnotherPrime() {
+        assertDoesNotThrow(() -> seedValidator.validate("7879"));
+    }
+
+
+    @Test
+    void shouldThrowWhenSeedIsNotPrime() {
+        BadRequestException ex = assertThrows(
+                BadRequestException.class,
+                () -> seedValidator.validate("10")  // 10 não é primo
+        );
+
+        assertEquals("Seed must be a prime number", ex.getMessage());
+    }
+
+    @Test
+    void shouldThrowWhenSeedIsOne() {
+        BadRequestException ex = assertThrows(
+                BadRequestException.class,
+                () -> seedValidator.validate("1")
+        );
+
+        assertEquals("Seed must be a prime number", ex.getMessage());
+    }
+
+    @Test
+    void shouldThrowWhenSeedIsZero() {
+        BadRequestException ex = assertThrows(
+                BadRequestException.class,
+                () -> seedValidator.validate("0")
+        );
+
+        assertEquals("Seed must be a prime number", ex.getMessage());
+    }
+
+    @Test
+    void shouldThrowWhenSeedIsNegative() {
+        BadRequestException ex = assertThrows(
+                BadRequestException.class,
+                () -> seedValidator.validate("-7")
+        );
+
+        assertEquals("Seed must be a prime number", ex.getMessage());
+    }
+
+
+    @Test
+    void shouldThrowWhenSeedIsNotNumeric() {
+        NumberFormatException ex = assertThrows(
+                NumberFormatException.class,
+                () -> seedValidator.validate("abc")
+        );
+
+        assertTrue(ex.getMessage().contains("For input string"));
+    }
+
+    @Test
+    void shouldThrowWhenSeedIsEmpty() {
+        NumberFormatException ex = assertThrows(
+                NumberFormatException.class,
+                () -> seedValidator.validate("")
+        );
+
+        assertTrue(ex.getMessage().contains("For input string"));
+    }
+
+    @Test
+    void shouldThrowWhenSeedIsNull() {
+        assertThrows(
+                NumberFormatException.class,
+                () -> seedValidator.validate(null)
+        );
+    }
+}
